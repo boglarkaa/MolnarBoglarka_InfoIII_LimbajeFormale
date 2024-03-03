113330a1cd3a23fe20098b399d062cf5de680403
Boglarka Molnar <114649140+boglarkaa@users.noreply.github.com>
2024-03-03 13:01:56 +0200

    Add files via upload

--- Lab3/Ex3/ex3.py
+++ Lab3/Ex3/ex3.py
@@ -1,47 +1,38 @@
 import re
 
 
-def read_file():
-    path = input("Enter path: ")
-    with open(path, "r") as file:
-        content = file.read()
-    return content
-
-
 def verification(content):
-
     # name starts with uppercase letter, followed by lovercase letters
-    name_format = re.compile(r"Name: [A-Za-z]{2,25}\s[A-Za-z-]{2,25}")
+    name_format = re.compile(r"First name: [A-Za-z]{2,25}\nLast name: [A-Za-z-]{2,25}")
     # phone nr exactly 10 digits
     phone_format = re.compile(r"Phone: \d{10}")
     # address: number, followed by street name
     address_format = re.compile(r"Address: \d{3}, [A-Za-z]{2,20} Street")
     # product name, price floating point nr, tva floating point nr + %, quantity: an integer number
     bill_format = re.compile(
-        r"Product: (.+), Price: (\d+\.\d{2}), TVA: (\d+\.\d{2})%, Quantity: (\d+)"
+        r"Service: (.+), Price: (\d+\.\d{2}), TVA: (\d+\.\d{2})%, Quantity: (\d+)"
     )
 
     errors = []
 
-    match_name = name_format.search(content)
-    if not match_name:
+    if not name_format.search(content):
         errors.append("Error at client name.")
 
-    match_phone = phone_format.search(content)
-    if not match_phone:
+    if not phone_format.search(content):
         errors.append("Error at phone number.")
 
-    match_address = address_format.search(content)
-    if not match_address:
+    if not address_format.search(content):
         errors.append("Error at address.")
 
-    match_produs = bill_format.findall(content)
-    if not match_produs:
-        errors.append("Error at product details.")
+    if not bill_format.search(content):
+        errors.append("Error at service details.")
     return errors
 
 
-content = read_file()
+path = input("Enter path: ")
+with open(path, "r") as file:
+    content = file.read()
+
 errors = verification(content)
 
 if not errors:
